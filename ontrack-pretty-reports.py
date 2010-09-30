1d45a13bf079de3806908db7986b4f779eaccb7a
Antonio <asantosv@gmail.com>
2010-09-30 23:26:50 +0200

    program skeleton

---
+++ ontrack-pretty-reports.py
@@ -0,0 +1,20 @@
+class Report:
+    """Set of daily reports"""
+    def to_html(self):
+        pass
+
+class DailyReport:
+    """Glucose level and medication intake in a certain day"""
+    def to_html(self):
+        pass
+
+def read_report(xml):
+    """Read the xml file and write a pretty html report
+
+    Arguments:
+    - `xml`: xml file
+    """
+    pass
+
+if __name__ == '__main__':
+    pass
