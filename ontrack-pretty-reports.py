c0a6ec17eb66b8903538adfc14e27059dc4a9a5d
Antonio <asantosv@gmail.com>
2010-10-16 23:07:37 +0200

    Write beginning and end of the html report. Refactoring

--- ontrack-pretty-reports.py
+++ ontrack-pretty-reports.py
@@ -1,40 +1,72 @@
 from optparse import OptionParser
 from xml.dom import minidom, Node
+from datetime import datetime
 
 import dateutil.parser
 
-class Record:
-    """A single record. A record may be a glucose one, or a medication one"""
-    def to_html(self):
-        pass
+class DailyRecord(dict):
+    """
+    Glucose level and medication intake in a certain day
+    """
+    def __init__(self, date_str):
+        self.date_str = date_str
 
-class DailyRecord(list):
-    """Glucose level and medication intake in a certain day (list single Record instances)"""
-    def to_html(self):
-        pass
+    def to_html(self, output):
+        print self.date_str
 
 class Report(dict):
-    """Report object. It is simply a dictionary of DailyRecords"""
-    def start_html(self):
+    """
+    Report object. It is simply a dictionary of DailyRecords
+    """
+    def start_html(self, output):
         """
         Write html report head
         """
-        pass
+        output.write("""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-ES" lang="es-ES">
+  <head>
+    <title>Diabetes Report - """ + datetime.now().__str__() + """</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    def end_html(self):
-        """Write html report ending
+    <link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+
+    <script src="js/jquery-1.4.2.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/jquery.scrollTo.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/main.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body>
+    <table cellspacing="0">
+      <caption>""" + sorted(self.keys())[0] + " - " + sorted(self.keys())[-1] +
+      """</caption>
+      <tbody>
+      """)
+
+    def end_html(self, output):
         """
-        pass
+        Write html report ending
+        """
+        output.write("</tbody>\n")
+        output.write("</body>\n")
+        output.write("</html>")
 
-    def to_html(self):
+    def to_html(self, file_out):
         """
         Write html report
         """
-        self.start_html()
-        self.end_html()
+        try:
+            output = open(file_out, "w")
+            self.start_html(output)
+            for day in sorted(self.keys()):
+                self[day].to_html(output)
+            self.end_html(output)
+            output.close()
+        except RuntimeError as e:
+            print "Error writing to file %s" % (file_out)
+            print e
 
 def parse_xml_report(input_file):
-    """Read the xml file and create the corresponding report
+    """
+    Read the xml file and create the corresponding report
 
     Arguments:
     - `input_file`: xml input file
@@ -42,17 +74,25 @@ def parse_xml_report(input_file):
     report = Report()
     xml_report = minidom.parse(input_file)
     record_list = xml_report.getElementsByTagName("record")
-    for record in record_list:
-        r = Record()
-        for node in record.childNodes:
+    for item in record_list:
+        r = {}
+        for node in item.childNodes:
             if node.nodeType == Node.ELEMENT_NODE:
-                setattr(r, node.nodeName, node.childNodes[0].nodeValue if node.childNodes else None)
-        date = dateutil.parser.parse(r.datetime)
+                r[node.nodeName] = node.childNodes[0].nodeValue if node.childNodes else None
+        date = dateutil.parser.parse(r["datetime"])
         date_str = date.date().__str__()
         if date_str not in report:
-            report[date_str] = DailyRecord()
+            report[date_str] = DailyRecord(date_str)
         dr = report[date_str]
-        dr.append(r)
+
+        r_category = r["category"]
+        r_type = r["type"]
+        r_value = r["value"]
+
+        if r_category not in dr:
+            dr[r_category] = {}
+        dr[r_category][r_type] = r_value
+
     return report
 
 def parse_options():
@@ -69,4 +109,4 @@ def parse_options():
 if __name__ == '__main__':
     options = parse_options()
     report = parse_xml_report(options.input)
-    report.to_html()
+    report.to_html(options.output)
