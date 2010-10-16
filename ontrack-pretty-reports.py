61b426c57a7b0cc0419d5bbd4d89d6fdb3921e5c
Antonio <asantosv@gmail.com>
2010-10-16 20:13:59 +0200

    parse the ontrack xml and create a Report instance

--- ontrack-pretty-reports.py
+++ ontrack-pretty-reports.py
@@ -1,24 +1,61 @@
 from optparse import OptionParser
+from xml.dom import minidom, Node
 
-class Report:
-    """Set of daily reports"""
+import dateutil.parser
+
+class Record:
+    """A single record. A record may be a glucose one, or a medication one"""
     def to_html(self):
         pass
 
-class DailyReport:
-    """Glucose level and medication intake in a certain day"""
+class DailyRecord(list):
+    """Glucose level and medication intake in a certain day (list single Record instances)"""
     def to_html(self):
         pass
 
-def read_report(xml):
-    """Read the xml file and write a pretty html report
+class Report(dict):
+    """Report object. It is simply a dictionary of DailyRecords"""
+    def start_html(self):
+        """
+        Write html report head
+        """
+        pass
+
+    def end_html(self):
+        """Write html report ending
+        """
+        pass
+
+    def to_html(self):
+        """
+        Write html report
+        """
+        self.start_html()
+        self.end_html()
+
+def parse_xml_report(input_file):
+    """Read the xml file and create the corresponding report
 
     Arguments:
-    - `xml`: xml file
+    - `input_file`: xml input file
     """
-    pass
+    report = Report()
+    xml_report = minidom.parse(input_file)
+    record_list = xml_report.getElementsByTagName("record")
+    for record in record_list:
+        r = Record()
+        for node in record.childNodes:
+            if node.nodeType == Node.ELEMENT_NODE:
+                setattr(r, node.nodeName, node.childNodes[0].nodeValue if node.childNodes else None)
+        date = dateutil.parser.parse(r.datetime)
+        date_str = date.date().__str__()
+        if date_str not in report:
+            report[date_str] = DailyRecord()
+        dr = report[date_str]
+        dr.append(r)
+    return report
 
-def parse_cli():
+def parse_options():
     parser = OptionParser()
     parser.add_option("-i", "--input", help="XML input file")
     parser.add_option("-o", "--output", help="HTML output file", default="report.html")
@@ -30,6 +67,6 @@ def parse_cli():
     return options
 
 if __name__ == '__main__':
-    options = parse_cli()
-    # read file and generate a Report
-    # Report to_html (foreach DailyReport in Report, to_html)
+    options = parse_options()
+    report = parse_xml_report(options.input)
+    report.to_html()
