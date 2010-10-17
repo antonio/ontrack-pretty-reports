1be261f17622b9afa2c84689f4096e1836381465
Antonio <asantosv@gmail.com>
2010-10-17 03:41:29 +0200

    writes xml contents as an html table

--- ontrack-pretty-reports.py
+++ ontrack-pretty-reports.py
@@ -3,6 +3,11 @@ from xml.dom import minidom, Node
 from datetime import datetime
 
 import dateutil.parser
+import codecs
+import ConfigParser
+
+# TODO: Statistics
+# TODO: Graphs
 
 class DailyRecord(dict):
     """
@@ -11,8 +16,55 @@ class DailyRecord(dict):
     def __init__(self, date_str):
         self.date_str = date_str
 
-    def to_html(self, output):
-        print self.date_str
+    def get_record_class(self, config, value):
+        """
+        Gets the corresponding 'td' class depending on the record value
+        Arguments:
+        - `config`: configuration
+        - `value`: value
+        """
+        if int(value) in range(int(config.get("limits", "low")), int(config.get("limits", "medium"))):
+            return "ok"
+        elif int(value) in range(int(config.get("limits", "medium")), int(config.get("limits", "high"))):
+            return "warning"
+        else:
+            return "alert"
+
+    def get_single_record_html(self, config, category, type):
+        """
+        Return the HTML code  corresponding to the given selection cell
+        Arguments:
+        - `category`: category of the record. May be one of the following
+           * breakfast
+           * after_breakfast
+           * lunch
+           * after_lunch
+           * dinner
+           * after_dinner
+        - `type`: glucose or medication
+        """
+        if config.get("strings", category) not in self or config.get("strings", type) not in self[config.get("strings", category)]:
+            return "<td class='no_value'>-</td>"
+        else:
+            value = self[config.get("strings", category)][config.get("strings", type)]
+            if type == "glucose":
+                return "<td class='%s'>%s</td>" % (self.get_record_class(config, value), value)
+            else:
+                return "<td class='no_value'>%s</td>" % (value)
+
+    def to_html(self, output, config):
+        output.write("<tr>")
+        output.write("<th scope='row'>" + self.date_str + "</th>")
+        output.write(self.get_single_record_html(config, "breakfast", "glucose"))
+        output.write(self.get_single_record_html(config, "breakfast", "medication"))
+        output.write(self.get_single_record_html(config, "after_breakfast", "glucose"))
+        output.write(self.get_single_record_html(config, "lunch", "glucose"))
+        output.write(self.get_single_record_html(config, "lunch", "medication"))
+        output.write(self.get_single_record_html(config, "after_lunch", "glucose"))
+        output.write(self.get_single_record_html(config, "dinner", "glucose"))
+        output.write(self.get_single_record_html(config, "dinner", "medication"))
+        output.write(self.get_single_record_html(config, "after_dinner", "glucose"))
+        output.write("</tr>")
 
 class Report(dict):
     """
@@ -36,10 +88,23 @@ class Report(dict):
   </head>
   <body>
     <table cellspacing="0">
-      <caption>""" + sorted(self.keys())[0] + " - " + sorted(self.keys())[-1] +
+      <caption>""" + sorted(self.keys())[0] + " .. " + sorted(self.keys())[-1] +
       """</caption>
       <tbody>
-      """)
+        <tr>
+        """)
+        output.write("<th scope='col' class='nobg'></th>")
+        output.write("<th scope='col'>" + config.get("strings", "breakfast") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "medication") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "after_breakfast") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "lunch") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "medication") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "after_lunch") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "dinner") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "medication") + "</th>")
+        output.write("<th scope='col'>" + config.get("strings", "after_dinner") + "</th>")
+        output.write("</tr>")
+
 
     def end_html(self, output):
         """
@@ -49,20 +114,19 @@ class Report(dict):
         output.write("</body>\n")
         output.write("</html>")
 
-    def to_html(self, file_out):
+    def to_html(self, file_out, config):
         """
         Write html report
         """
         try:
-            output = open(file_out, "w")
+            output = codecs.open(file_out, "w", "utf8")
             self.start_html(output)
             for day in sorted(self.keys()):
-                self[day].to_html(output)
+                self[day].to_html(output, config)
             self.end_html(output)
             output.close()
-        except RuntimeError as e:
+        except RuntimeError:
             print "Error writing to file %s" % (file_out)
-            print e
 
 def parse_xml_report(input_file):
     """
@@ -99,6 +163,7 @@ def parse_options():
     parser = OptionParser()
     parser.add_option("-i", "--input", help="XML input file")
     parser.add_option("-o", "--output", help="HTML output file", default="report.html")
+    parser.add_option("-c", "--config", help="Configuration file", default="config")
     (options, args) = parser.parse_args()
     if options.input == None :
         print("\nERROR: Please specify an input file\n")
@@ -108,5 +173,10 @@ def parse_options():
 
 if __name__ == '__main__':
     options = parse_options()
+    config = ConfigParser.SafeConfigParser()
+    try:
+        config.readfp(codecs.open(options.config, "r", "utf8"))
+    except IOError:
+        print "Error reading configuration file"
     report = parse_xml_report(options.input)
-    report.to_html(options.output)
+    report.to_html(options.output, config)
